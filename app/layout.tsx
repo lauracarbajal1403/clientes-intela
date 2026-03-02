01b5a51ac9b88527d0997fc40b31bdcad95f575c
laura <laura.carbajal9912@alumnos.udg.mx>
2026-03-02 11:46:45 -0600

    QQ

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,30 +42,9 @@ export default function RootLayout({
       </head>
       <body className={`font-sans antialiased`}>
         {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-SD0Y7NY18T"
-          strategy="afterInteractive"
-        />
-        <Script id="ga-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-SD0Y7NY18T');
-          `}
-        </Script>
-
-        {/* Google Tag Manager */}
-        <Script id="gtm-init" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5DZBHBTF');
-          `}
-        </Script>
-
+        <GoogleTagManager gtmId="GTM-5DZBHBTF" />
+        <GoogleAnalytics gaId="G-SD0Y7NY18T" />
+        
         {children}
         
         {/*
